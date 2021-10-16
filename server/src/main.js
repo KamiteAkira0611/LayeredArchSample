c736aa29b0d5bd3a97ca59b54d7b2798dd2a714a
KamiteAkira0611 <kamiteakira@gmail.com>
2021-10-16 13:25:51 +0900

    save

--- server/src/main.js
+++ server/src/main.js
@@ -6,14 +6,6 @@ app.get("/", (req, res) =>{
   res.sendFile(`${process.cwd()}/public/index.html`);
 });
 
-app.get("/users", (req, res) =>{
-  res.json([{
-    id: 1, name: "maeda"
-  }, {
-    id: 2, name: "takada"
-  }])
-});
-
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
 });
