a2c505834a71e4a8d88fc228044c9fb6aca56d9f
KamiteAkira0611 <kamiteakira@gmail.com>
2021-11-02 22:20:57 +0900

    base complete

--- front/src/pages/recipes/index.tsx
+++ front/src/pages/recipes/index.tsx
@@ -1,4 +1,4 @@
-import { useQuery, gql } from '@apollo/client'
+import { useQuery, gql, useMutation } from '@apollo/client'
 import { Recipe } from 'src/graphql/types'
 
 const GET_RECIPES = gql`
@@ -12,7 +12,7 @@ const GET_RECIPES = gql`
 `
 
 const Recipes = () => {
-  const { loading, error, data } = useQuery(GET_RECIPES)
+  const {data, error, loading} = useQuery(GET_RECIPES)
 
   if(loading) return <p>loading</p>
   if(error) return <p>error</p>
@@ -20,17 +20,10 @@ const Recipes = () => {
   return (
     <div>
       <h1>Recipes</h1>
-      <input type="text" />
-      <input type="text" />
-      <button>追加</button>
       {data.recipes.map((recipe: Recipe, i: number) => {
         return(
           <div key={i}>
             <p>ID:{recipe.id} {recipe.title} {recipe.description}</p>
-            <input type="text" />
-            <input type="text" />
-            <button>変更</button>
-            <button>削除</button>
           </div>
         )
       })}
